edc2d8a351fb4dafed77e38e7a48476e77231126
EyosiyasGe <eyosiyasgeze@gmail.com>
2025-06-05 22:37:26 +0300

    Forth week

---
+++ techcon_website/hello.py
@@ -0,0 +1,9 @@
+import random
+a={1,2,3,4,5,6,7,8,9,10}
+print("the random number is ", random.choice(list(a)))
+
+import os  # Module for interacting with the operating system
+
+# Get the current working directory
+cwd = os.getcwd()
+print(cwd)
